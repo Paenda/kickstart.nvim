351b18c33144215e30dfb69a9f7ea5ea426ca37b
panda <nithitsathornkit@proton.me>
2026-01-21 20:24:21 +0100

    Add google-java-format to conform

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -30,8 +30,9 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
+      java = { 'google-java-format' },
       python = { 'black' },
+      -- Conform can also run multiple formatters sequentially
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -219,7 +219,6 @@ return {
         rust_analyzer = {},
         -- hls = {},
         -- omnisharp = {},
-        -- jdtls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
